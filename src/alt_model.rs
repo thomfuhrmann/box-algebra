302d2d9d32e26f59c37637aa3281a1580bb59965
Thomas Fuhrmann <thom.fuhrmann@gmail.com>
2026-05-27 11:25:17 +0200

    refactor: add flag to indicate if box is ordered

--- src/alt_model.rs
+++ src/alt_model.rs
@@ -41,14 +41,21 @@ impl From<usize> for BoxId {
 /// Raw uncomitted box data
 #[derive(Debug)]
 pub struct RawBox<'a> {
+    pub is_ordered: &'a [bool],
     pub colors: &'a [Color],
     pub multiplicities: &'a [Natural],
     pub lengths: &'a [u32],
 }
 
 impl<'a> RawBox<'a> {
-    pub fn new(colors: &'a [Color], multiplicities: &'a [Natural], lengths: &'a [u32]) -> Self {
+    pub fn new(
+        is_ordered: &'a [bool],
+        colors: &'a [Color],
+        multiplicities: &'a [Natural],
+        lengths: &'a [u32],
+    ) -> Self {
         Self {
+            is_ordered,
             colors,
             multiplicities,
             lengths,
@@ -57,10 +64,36 @@ impl<'a> RawBox<'a> {
 
     pub fn hash(&self, random_state: &RandomState) -> u64 {
         let mut hasher = random_state.build_hasher();
+        self.is_ordered.hash(&mut hasher);
         self.colors.hash(&mut hasher);
         self.multiplicities.hash(&mut hasher);
         self.lengths.hash(&mut hasher);
         hasher.finish()
+    }
+}
+
+/// Raw uncomitted box data
+#[derive(Debug)]
+pub struct RawBoxMut<'a> {
+    pub is_ordered: &'a mut [bool],
+    pub colors: &'a mut [Color],
+    pub multiplicities: &'a mut [Natural],
+    pub lengths: &'a mut [u32],
+}
+
+impl<'a> RawBoxMut<'a> {
+    pub fn new(
+        is_ordered: &'a mut [bool],
+        colors: &'a mut [Color],
+        multiplicities: &'a mut [Natural],
+        lengths: &'a mut [u32],
+    ) -> Self {
+        Self {
+            is_ordered,
+            colors,
+            multiplicities,
+            lengths,
+        }
     }
 }
 
@@ -101,6 +134,8 @@ impl Mul<Color> for Color {
 pub struct BoxArena {
     /// Box colors
     pub colors: Vec<Color>,
+    /// Flag to indicate if box is ordered
+    pub is_ordered: Vec<bool>,
     /// Multiplicities of boxes
     pub multiplicities: Vec<Natural>,
     /// Numbers of rows occupied by boxes
@@ -134,6 +169,7 @@ impl BoxArena {
         let random_state = RandomState::new();
         let mut cache = RapidHashMap::new();
 
+        let mut is_ordered = Vec::with_capacity(ARENA_INIT_CAPACITY);
         let mut colors = Vec::with_capacity(ARENA_INIT_CAPACITY);
         let mut multiplicities = Vec::with_capacity(ARENA_INIT_CAPACITY);
         let mut lengths = Vec::with_capacity(ARENA_INIT_CAPACITY);
@@ -147,6 +183,7 @@ impl BoxArena {
             cache.insert(hash, id);
             expression_roots.push(id);
 
+            is_ordered.extend_from_slice(raw_box.is_ordered);
             colors.extend_from_slice(raw_box.colors);
             multiplicities.extend_from_slice(raw_box.multiplicities);
             lengths.extend_from_slice(raw_box.lengths);
@@ -154,15 +191,16 @@ impl BoxArena {
 
         register_box(
             Self::ZERO,
-            RawBox::new(&[Color::Black], &[Natural::from(1_u32)], &[1]),
+            RawBox::new(&[false], &[Color::Black], &[Natural::from(1_u32)], &[1]),
         );
         register_box(
             Self::ANTI_ZERO,
-            RawBox::new(&[Color::Red], &[Natural::from(1_u32)], &[1]),
+            RawBox::new(&[false], &[Color::Red], &[Natural::from(1_u32)], &[1]),
         );
         register_box(
             Self::ONE,
             RawBox::new(
+                &[false, false],
                 &[Color::Black, Color::Black],
                 &[Natural::from(1_u32), Natural::from(1_u32)],
                 &[2, 1],
@@ -171,6 +209,7 @@ impl BoxArena {
         register_box(
             Self::ANTI_ONE,
             RawBox::new(
+                &[false, false],
                 &[Color::Red, Color::Black],
                 &[Natural::from(1_u32), Natural::from(1_u32)],
                 &[2, 1],
@@ -179,6 +218,7 @@ impl BoxArena {
         register_box(
             Self::NEG_ONE,
             RawBox::new(
+                &[false, false],
                 &[Color::Black, Color::Red],
                 &[Natural::from(1_u32), Natural::from(1_u32)],
                 &[2, 1],
@@ -187,6 +227,7 @@ impl BoxArena {
         register_box(
             Self::ANTI_NEG_ONE,
             RawBox::new(
+                &[false, false],
                 &[Color::Red, Color::Red],
                 &[Natural::from(1_u32), Natural::from(1_u32)],
                 &[2, 1],
@@ -195,6 +236,7 @@ impl BoxArena {
         register_box(
             Self::ALPHA,
             RawBox::new(
+                &[false, false, false],
                 &[Color::Black, Color::Black, Color::Black],
                 &[
                     Natural::from(1_u32),
@@ -206,6 +248,7 @@ impl BoxArena {
         );
 
         Self {
+            is_ordered,
             colors,
             multiplicities,
             lengths,
@@ -226,6 +269,7 @@ impl BoxArena {
         let idx = box_id.index();
         let len = self.lengths[idx] as usize;
         RawBox {
+            is_ordered: &self.is_ordered[idx..(idx + len)],
             colors: &self.colors[idx..(idx + len)],
             multiplicities: &self.multiplicities[idx..(idx + len)],
             lengths: &self.lengths[idx..(idx + len)],
@@ -239,7 +283,8 @@ impl BoxArena {
         // cache hit
         if let Some(&existing_id) = self.cache.get(&hash) {
             let view = self.get(existing_id);
-            if view.colors == raw.colors
+            if view.is_ordered == raw.is_ordered
+                && view.colors == raw.colors
                 && view.multiplicities == raw.multiplicities
                 && view.lengths == raw.lengths
             {
@@ -252,6 +297,7 @@ impl BoxArena {
         self.cache.insert(hash, new_id);
         self.expression_roots.push(new_id);
 
+        self.is_ordered.extend_from_slice(raw.is_ordered);
         self.colors.extend_from_slice(raw.colors);
         self.multiplicities.extend_from_slice(raw.multiplicities);
         self.lengths.extend_from_slice(raw.lengths);
@@ -260,10 +306,17 @@ impl BoxArena {
     }
 
     /// Wraps an existing expression in a new box container
-    pub fn wrap_in_box(&mut self, source: BoxId, color: Color, multiplicity: Natural) -> BoxId {
+    pub fn wrap_in_box(
+        &mut self,
+        source: BoxId,
+        ordered: bool,
+        color: Color,
+        multiplicity: Natural,
+    ) -> BoxId {
         let source_idx = source.index();
         let source_len = self.lengths[source_idx] as usize;
 
+        let mut is_ordered = vec![ordered];
         let mut colors = vec![color];
         let mut multiplicities = vec![Natural::from(1_u32)];
         let mut lengths = vec![1 + source_len as u32];
@@ -272,6 +325,7 @@ impl BoxArena {
         for i in 0..source_len {
             let curr_idx = source_idx + i;
 
+            is_ordered.push(self.is_ordered[curr_idx]);
             colors.push(self.colors[curr_idx]);
 
             if i == 0 {
@@ -283,29 +337,31 @@ impl BoxArena {
             lengths.push(self.lengths[curr_idx]);
         }
 
-        let raw = RawBox::new(&colors, &multiplicities, &lengths);
+        let raw = RawBox::new(&is_ordered, &colors, &multiplicities, &lengths);
 
         self.commit(raw)
     }
 
     pub fn from_u32(&mut self, num: u32) -> BoxId {
-        self.wrap_in_box(BoxArena::ZERO, Color::Black, Natural::from(num))
+        self.wrap_in_box(BoxArena::ZERO, false, Color::Black, Natural::from(num))
     }
 
     pub fn from_u64(&mut self, num: u64) -> BoxId {
-        self.wrap_in_box(BoxArena::ZERO, Color::Black, Natural::from(num))
+        self.wrap_in_box(BoxArena::ZERO, false, Color::Black, Natural::from(num))
     }
 
     pub fn from_i32(&mut self, num: i32) -> BoxId {
         if num < 0 {
             self.wrap_in_box(
                 BoxArena::ANTI_ZERO,
+                false,
                 Color::Black,
                 Natural::from(num.unsigned_abs()),
             )
         } else {
             self.wrap_in_box(
                 BoxArena::ZERO,
+                false,
                 Color::Black,
                 Natural::from(num.unsigned_abs()),
             )
@@ -316,36 +372,43 @@ impl BoxArena {
         if num < 0 {
             self.wrap_in_box(
                 BoxArena::ANTI_ZERO,
+                false,
                 Color::Black,
                 Natural::from(num.unsigned_abs()),
             )
         } else {
             self.wrap_in_box(
                 BoxArena::ZERO,
+                false,
                 Color::Black,
                 Natural::from(num.unsigned_abs()),
             )
         }
     }
 
+    /// Returns if box is ordered
+    pub fn is_box_ordered(&self, box_id: BoxId) -> bool {
+        self.is_ordered[box_id.index()]
+    }
+
     /// Returns the outer color of a box
-    pub fn get_box_color(&self, box_id: BoxId) -> Color {
+    pub fn box_color(&self, box_id: BoxId) -> Color {
         self.colors[box_id.index()]
     }
 
     /// Returns the arity of a box
-    pub fn get_box_multiplicity(&self, box_id: BoxId) -> Natural {
+    pub fn box_multiplicity(&self, box_id: BoxId) -> Natural {
         self.multiplicities[box_id.index()].clone()
     }
 
     /// Returns the number of rows occupied by this box (including itself)
-    pub fn get_box_len(&self, box_id: BoxId) -> u32 {
+    pub fn box_len(&self, box_id: BoxId) -> u32 {
         self.lengths[box_id.index()]
     }
 
     /// Computes the arity of a box
-    pub fn arity(&self, box_id: BoxId) -> u32 {
-        let len = self.get_box_len(box_id);
+    pub fn box_arity(&self, box_id: BoxId) -> u32 {
+        let len = self.box_len(box_id);
         if len == 0 {
             return 0;
         }
@@ -363,8 +426,8 @@ impl BoxArena {
 
     /// Checks if two boxes have the same content structure (ignoring their outer outer colors and multiplicities)
     pub fn equal_content(&self, left: BoxId, right: BoxId) -> bool {
-        let left_len = self.get_box_len(left) as usize;
-        let right_len = self.get_box_len(right) as usize;
+        let left_len = self.box_len(left) as usize;
+        let right_len = self.box_len(right) as usize;
 
         if left_len != right_len {
             return false;
@@ -381,7 +444,8 @@ impl BoxArena {
 
         self.colors[left_inner.clone()] == self.colors[right_inner.clone()]
             && self.multiplicities[left_inner] == self.multiplicities[right_inner]
-            && self.lengths[left_range] == self.lengths[right_range]
+            && self.lengths[left_range.clone()] == self.lengths[right_range.clone()]
+            && self.is_ordered[left_range] == self.is_ordered[right_range]
     }
 
     /// Hashes the content of a box (ignoring its outer color and multiplicity)
@@ -389,8 +453,9 @@ impl BoxArena {
         let mut hasher = self.random_state.build_hasher();
 
         let start = box_id.index();
-        let len = self.get_box_len(box_id) as usize;
+        let len = self.box_len(box_id) as usize;
 
+        self.is_ordered[start..(start + len)].hash(&mut hasher);
         self.colors[start + 1..(start + len)].hash(&mut hasher);
         self.multiplicities[start + 1..(start + len)].hash(&mut hasher);
         self.lengths[start..(start + len)].hash(&mut hasher);
@@ -398,12 +463,12 @@ impl BoxArena {
         hasher.finish()
     }
 
-    fn sort_box_content(colors: &mut [Color], multiplicities: &mut [Natural], lengths: &mut [u32]) {
-        if lengths.is_empty() {
+    pub fn sort_box_content(raw_box: &mut RawBoxMut) {
+        if raw_box.lengths.is_empty() {
             return;
         }
 
-        let total_box_len = lengths[0] as usize;
+        let total_box_len = raw_box.lengths[0] as usize;
         if total_box_len <= 1 {
             return;
         }
@@ -415,7 +480,7 @@ impl BoxArena {
         let mut child_meta = Vec::new();
         let mut curr = start_idx;
         while curr < end_idx {
-            let len = lengths[curr] as usize;
+            let len = raw_box.lengths[curr] as usize;
             child_meta.push((curr, len));
             curr += len;
         }
@@ -429,51 +494,54 @@ impl BoxArena {
             let range_a = start_a..(start_a + len_a);
             let range_b = start_b..(start_b + len_b);
 
-            let len_cmp = lengths[range_a.clone()].cmp(&lengths[range_b.clone()]);
+            let len_cmp = raw_box.lengths[range_a.clone()].cmp(&raw_box.lengths[range_b.clone()]);
             if len_cmp != Equal {
                 return len_cmp;
             }
 
-            let col_cmp = colors[range_a.clone()].cmp(&colors[range_b.clone()]);
+            let col_cmp = raw_box.colors[range_a.clone()].cmp(&raw_box.colors[range_b.clone()]);
             if col_cmp != Equal {
                 return col_cmp;
             }
 
-            multiplicities[range_a].cmp(&multiplicities[range_b])
+            raw_box.multiplicities[range_a].cmp(&raw_box.multiplicities[range_b])
         });
 
         // Permute values into temporary scratch space
         let payload_len = end_idx - start_idx;
+        let mut sorted_is_ordered = Vec::with_capacity(payload_len);
         let mut sorted_colors = Vec::with_capacity(payload_len);
         let mut sorted_lens = Vec::with_capacity(payload_len);
         let mut sorted_mults = Vec::with_capacity(payload_len);
 
         for &(start, len) in &child_meta {
             let range = start..(start + len);
-            sorted_colors.extend_from_slice(&colors[range.clone()]);
-            sorted_lens.extend_from_slice(&lengths[range.clone()]);
+            sorted_is_ordered.extend_from_slice(&raw_box.is_ordered[range.clone()]);
+            sorted_colors.extend_from_slice(&raw_box.colors[range.clone()]);
+            sorted_lens.extend_from_slice(&raw_box.lengths[range.clone()]);
 
             for idx in range {
-                let item = std::mem::take(&mut multiplicities[idx]);
+                let item = std::mem::take(&mut raw_box.multiplicities[idx]);
                 sorted_mults.push(item);
             }
         }
 
         let target_range = start_idx..end_idx;
-        colors[target_range.clone()].copy_from_slice(&sorted_colors);
-        lengths[target_range.clone()].copy_from_slice(&sorted_lens);
+        raw_box.is_ordered[target_range.clone()].copy_from_slice(&sorted_is_ordered);
+        raw_box.colors[target_range.clone()].copy_from_slice(&sorted_colors);
+        raw_box.lengths[target_range.clone()].copy_from_slice(&sorted_lens);
 
         for (dest_idx, src_natural) in target_range.zip(sorted_mults) {
-            multiplicities[dest_idx] = src_natural;
+            raw_box.multiplicities[dest_idx] = src_natural;
         }
     }
 
-    /// Adds two boxes
+    /// Adds two boxes (unordered)
     pub fn add(&mut self, lhs: BoxId, rhs: BoxId) -> BoxId {
         let mut unique_children: RapidHashMap<u64, (BoxId, Color, Natural)> = RapidHashMap::new();
 
         let mut add_child_boxes = |box_id @ BoxId(start_idx): BoxId| {
-            let box_len = self.get_box_len(box_id);
+            let box_len = self.box_len(box_id);
             let mut curr = start_idx + 1;
             let end = start_idx + box_len;
 
@@ -517,14 +585,16 @@ impl BoxArena {
         add_child_boxes(lhs);
         add_child_boxes(rhs);
 
+        let mut is_ordered = vec![];
         let mut colors = vec![];
         let mut multiplicities = vec![];
         let mut lengths = vec![];
 
-        let lhs_col = self.get_box_color(lhs);
-        let rhs_col = self.get_box_color(rhs);
+        let lhs_col = self.box_color(lhs);
+        let rhs_col = self.box_color(rhs);
         let final_color = lhs_col + rhs_col;
 
+        is_ordered.push(false);
         colors.push(final_color);
         multiplicities.push(Natural::from(1_u32));
         lengths.push(0);
@@ -538,10 +608,11 @@ impl BoxArena {
             }
 
             let start = id.id();
-            let len = self.get_box_len(*id);
+            let len = self.box_len(*id);
             for i in 0..len {
                 let src_idx = start + i;
 
+                is_ordered.push(self.is_ordered[src_idx as usize]);
                 colors.push(*col);
 
                 if i == 0 {
@@ -558,7 +629,7 @@ impl BoxArena {
         // update total length
         lengths[0] = (1 + written_len) as u32;
 
-        let raw = RawBox::new(&colors, &multiplicities, &lengths);
+        let raw = RawBox::new(&is_ordered, &colors, &multiplicities, &lengths);
         self.commit(raw)
     }
 }
@@ -613,14 +684,24 @@ mod tests {
         let zero = BoxArena::ZERO;
 
         let two = arena.add(BoxArena::ONE, BoxArena::ONE);
-        let expected = arena.wrap_in_box(zero, Color::Black, Natural::from(2_u32));
+        let expected = arena.wrap_in_box(zero, false, Color::Black, Natural::from(2_u32));
         assert_eq!(two, expected);
 
         let minus_two = arena.add(BoxArena::NEG_ONE, BoxArena::NEG_ONE);
-        let expected = arena.wrap_in_box(BoxArena::ANTI_ZERO, Color::Black, Natural::from(2_u32));
+        let expected = arena.wrap_in_box(
+            BoxArena::ANTI_ZERO,
+            false,
+            Color::Black,
+            Natural::from(2_u32),
+        );
         assert_eq!(minus_two, expected);
 
-        let minus_one = arena.wrap_in_box(BoxArena::ANTI_ZERO, Color::Black, Natural::from(1_u32));
+        let minus_one = arena.wrap_in_box(
+            BoxArena::ANTI_ZERO,
+            false,
+            Color::Black,
+            Natural::from(1_u32),
+        );
         let zero = arena.add(minus_one, BoxArena::ONE);
         assert_eq!(zero, BoxArena::ZERO);
 
@@ -638,13 +719,13 @@ mod tests {
 
         let alpha = BoxArena::ALPHA;
         let two_alpha = arena.add(alpha, alpha);
-        let expected = arena.wrap_in_box(BoxArena::ONE, Color::Black, Natural::from(2_u32));
+        let expected = arena.wrap_in_box(BoxArena::ONE, false, Color::Black, Natural::from(2_u32));
         assert_eq!(two_alpha, expected);
 
-        let alpha_1 = arena.wrap_in_box(alpha, Color::Black, Natural::from(1_u32));
-        let two_alpha_1 = arena.wrap_in_box(alpha, Color::Black, Natural::from(2_u32));
+        let alpha_1 = arena.wrap_in_box(alpha, false, Color::Black, Natural::from(1_u32));
+        let two_alpha_1 = arena.wrap_in_box(alpha, false, Color::Black, Natural::from(2_u32));
         let sum = arena.add(alpha_1, two_alpha_1);
-        let expected = arena.wrap_in_box(alpha, Color::Black, Natural::from(3_u32));
+        let expected = arena.wrap_in_box(alpha, false, Color::Black, Natural::from(3_u32));
         assert_eq!(sum, expected);
     }
 }
